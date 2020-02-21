8a70cae26d1cf17bade379fb73cd49d24ea3dd94
Pruthivika <pypvika1@gmail.com>
2020-02-21 09:19:30 +0530

    added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    AdminComponent,
+    HomeComponent
+   
+   
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyCCSx64xOU4Yvoa2NiObumMicke45YDfgk",
+    authDomain: "angularfirestore-project1.firebaseapp.com",
+    databaseURL: "https://angularfirestore-project1.firebaseio.com",
+    projectId: "angularfirestore-project1",
+    storageBucket: "angularfirestore-project1.appspot.com",
+    messagingSenderId: "617533935127",
+    appId: "1:617533935127:web:88222c0d5fa4caa3a5d652",
+    measurementId: "G-V5F2772LKL"
+  }
 };
 
 /*
