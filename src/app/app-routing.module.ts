58b836621c5c1d616d71057f90a9f3bd87a75436
Pruthivika <pypvika1@gmail.com>
2020-02-25 18:22:04 +0530

    added basic components and configured with particle.js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component:HomeComponent},
+  { path: '', redirectTo: '/index', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
